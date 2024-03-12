0d0dadc1b9947afb0a4104beea37abb51fa17263
JT Rael <raeljohntimothy@gmail.com>
2024-03-13 00:34:30 +0800

    Modified and applied the imports

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,12 +1,16 @@
-import { View } from 'react-native';
+import { FlatList, View } from 'react-native';
 import products from '@assets/data/products';
 import ProductListItem from '@components/ProducListItem';
 
 export default function MenuScreen() {
   return (
     <View>
-      <ProductListItem product={products[0]}/>
-      <ProductListItem product={products[1]}/>
+      {/* <ProductListItem product={products[0]}/>
+      <ProductListItem product={products[1]}/> */}
+      <FlatList 
+        data={products}
+        renderItem={({ item }) => <ProductListItem product={item}/>}
+      />
     </View>
   );
 }
