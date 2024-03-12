b26ffb26d8ed3765a6c9cd4e9449841179190429
JT Rael <raeljohntimothy@gmail.com>
2024-03-13 00:05:01 +0800

    First custom component ProductListItem

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Setup
 - create src/ and move inside the DIRs: app, components, constants
+
+# Practices
+- Inside the src should be app, components, constants
+- Have one component per file
---
+++ src/components/ProducListItem.tsx
@@ -0,0 +1,35 @@
+import Colors from '@/src/constants/Colors';
+import { Text, View, StyleSheet, Image } from 'react-native';
+
+const ProductListItem = ({ product }) => {
+    return (
+        <View style={styles.container}>
+            <Image source={{ uri: product.image }} style={styles.image} />
+            <Text style={styles.title}>{product.name}</Text>
+            <Text style={styles.price}>{product.price}</Text>
+        </View>
+    )
+}
+
+export default ProductListItem
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'white',
+        padding: 10,
+        borderRadius: 20
+    },
+    image: {
+        width: '100%',
+        aspectRatio: 1
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: '600',
+        marginVertical: 10
+    },
+    price: {
+        color: Colors.light.tint,
+        fontWeight: 'bold'
+    }
+});
