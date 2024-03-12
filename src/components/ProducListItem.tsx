ed4d46876a480e02d8178e4f7376ff238e468ffe
JT Rael <raeljohntimothy@gmail.com>
2024-03-13 00:48:49 +0800

    Flatlist and custom render styling

--- src/components/ProducListItem.tsx
+++ src/components/ProducListItem.tsx
@@ -11,7 +11,16 @@ type ProductListItemProps = {
 const ProductListItem = ({ product }: ProductListItemProps) => {
     return (
         <View style={styles.container}>
-            <Image source={{ uri: product.image || defaultPizzaImage }} style={styles.image} />
+            {/* 
+                resizeMode's default is 'cover' meaning cover the whole area with image
+                while 'contain' adjusts itself to see the whole image 
+            */}
+            <Image 
+                source={{ uri: product.image || defaultPizzaImage }} 
+                style={styles.image} 
+                resizeMode='contain'
+            />
+            
             <Text style={styles.title}>{product.name}</Text>
             <Text style={styles.price}>{product.price}</Text>
         </View>
@@ -24,7 +33,9 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: 'white',
         padding: 10,
-        borderRadius: 20
+        borderRadius: 20,
+        flex: 1, // Split equally the space amongst its siblings
+        maxWidth: '50%'
     },
     image: {
         width: '100%',
