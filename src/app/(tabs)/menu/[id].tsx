0d1eec4b0cc4096c10aaf5adb102ee7fcd9a3277
JT Rael <raeljohntimothy@gmail.com>
2024-03-13 10:48:32 +0800

    Product detail screen

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,15 +1,111 @@
 import { Stack, useLocalSearchParams } from 'expo-router'
-import { View, Text } from 'react-native'
+import { Image, View, Text, StyleSheet, Pressable } from 'react-native'
+import products from '@assets/data/products'
+import { defaultPizzaImage } from '@/components/ProducListItem'
+import { useState } from 'react'
+import Button from '@/components/Button'
+
+const sizes = ['S', 'M', 'L', 'XL']
 
 const ProductDetailsScreen = () => {
-    const { id } = useLocalSearchParams()
+    const { id } = useLocalSearchParams() // Returns string type
+
+    const [selectedSize, setSelectedSize] = useState('M')
+
+    const product = products.find((p) => p.id.toString() === id)
+
+    const addToCart = () => {
+        console.warn('Adding to cart, size:', selectedSize)
+    }
+
+    if (!product) {
+        return <Text>Product not found</Text>
+    }
 
     return (
-        <View>
-            <Stack.Screen options={{ title: 'Details:' + id }} />
-            <Text>ProductDetailsScreen for id: {id}</Text>
+        <View style={styles.container}>
+            {/* To customize the current screen */}
+            <Stack.Screen options={{ title: product.name }} />
+
+            <Image
+                source={{ uri: product.image || defaultPizzaImage }}
+                style={styles.image}
+            />
+
+            <Text>Select Size</Text>
+            <View style={styles.sizes}>
+                {sizes.map((size) => (
+                    <Pressable
+                        onPress={() => {setSelectedSize(size)}} 
+                        style={[
+                            styles.size, 
+                            { 
+                                backgroundColor: selectedSize === size ? 'gainsboro' : 'white'
+                            }
+                        ]} 
+                        key={size}
+                    >
+                        <Text 
+                            style={[
+                                styles.sizeText,
+                                {
+                                    color: selectedSize === size ? 'black' : 'gray'
+                                }
+                            ]}
+                        >
+                            {size}
+                        </Text>
+                    </Pressable>
+                ))}
+            </View>
+
+            <Text style={styles.price}>${product.price}</Text>
+            <Button onPress={addToCart} text="Add to cart" />
         </View>
     )
 }
 
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'white',
+        flex: 1,
+        padding: 10
+    },
+    image: {
+        width: '100%',
+        aspectRatio: 1
+    },
+    price: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginTop: 'auto'
+    },
+    sizes: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginVertical: 10 
+    },
+    size: {
+        backgroundColor: 'gainsboro',
+        width: 50,
+        aspectRatio: 1,
+        borderRadius: 25, // half of the width or height,
+        alignItems: 'center', // center item horizontally
+        justifyContent: 'center' // center item vertically
+    },
+    sizeText: {
+        fontSize: 20,
+        fontWeight: 'bold'
+    }
+})
+
 export default ProductDetailsScreen
+
+/*
+    aspectRatio
+    -   it instructs the layout system to make the element a square, 
+        where the width and height are equal.
+
+    justifyContent
+    -   available space will be shared around the items
+*/
--- src/app/(tabs)/menu/_layout.tsx
+++ src/app/(tabs)/menu/_layout.tsx
@@ -6,7 +6,8 @@ export default function MenuStack() {
             <Stack.Screen name="index" options={{ title: 'Menu'}} />
             {/* 
                 Or go inside the index and put <Stack.Screen options={{ title: 'Menu'}} />
-                'name' can be ommitted for you are already inside the file
+                'name' can be ommitted for you are already inside the file just like in 
+                [id].tsx
             */}
         </Stack>
     )
